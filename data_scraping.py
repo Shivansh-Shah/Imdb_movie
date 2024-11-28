a4cffd5479758d5aa52e4f948fd4b9dabb7871e3
ShaiviNandi <shaivi.nandi@gmail.com>
2024-11-28 20:57:07 +0530

    Add files via upload

---
+++ data_scraping.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+
+def getMovieID(movieName):
+    url="https://www.ratingraph.com/search-results/"
+    
+    for i in movieName.split(" "):
+            url=url+i+"%20"
+    movie=requests.get(url)
+    if movie.status_code==200:
+        soup=BeautifulSoup(movie.text,'html.parser')
+        data=soup.find_all('div',class_='titles results')
+        return(float((data[0].find_all('a')[0].get('href')).split('-')[-1][0:-1]))
+    else:
+         return -1
+    
+def getGraphData(movieID):
+      url="https://www.ratingraph.com/movie-history/"+movieID+'/'
+      response = requests.get(url)
+      if response.status_code == 200:
+            data = response.json()
+            return(data[1]['data']) 
+      else:
+            return -1
+
+def getMovieData(movieName):
+    url="https://www.omdbapi.com/?t="
+    for i in movieName.split():
+        url=url+i+"+"
+    url=url[:len(url)-1]+"&plot=full&apikey=c2403270"
+    response=requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return(data) 
+    else:
+        return -1
+
+
+
+
+
+
+# print((getGraphData((getMovieID("3 Idiots")))))
+# print(getMovieData((("3 idiots"))))
+
+# print(type(getMovieData((("3 idiots")))))
+# print(type(getMovieID("3 Idiots")))
