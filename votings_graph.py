657ed73cecf52af66039282cd511666a61747bd1
saratcbharadwaj <Sarat.Bharadwaj@iiitb.ac.in>
2024-12-08 18:26:13 +0530

    Update votings_graph.py by adding comments

--- votings_graph.py
+++ votings_graph.py
@@ -1,19 +1,25 @@
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plot #Module for graphs
 import numpy as np
+# The below two modules imported are necessary for datetime conversions.
 import matplotlib.dates as mdates
 from datetime import datetime
 
+#This is the data on which the graphs are based on
 from datascraped import GraphData
+
+# A function that generates a graph plotting total_votes from 2020 on y-axis against Dates on x-axis.
 def generate_graph():
+
+    # A function that returns a tuple containing figure and axes
     fig, ax = plot.subplots()
-    
-    display_x = [d['x'] for d in GraphData]
+    # This list contains all the elements in d['total_votes'] with their commas(,) replaced with '' .
+    # The above conversion is done for display points on y axis ( looks clean ).
     display_y = [int(d['total_votes'].replace(',', '')) / 1000 for d in GraphData] 
-
-    xpoints = np.array([datetime.fromtimestamp(d / 1000) for d in display_x] )
-
+    #Conversion into seconds and then storing all the elements d['x'] in the form of an array
+    xpoints = np.array([datetime.fromtimestamp(d['x'] / 1000) for d in GraphData] )
+    #Consists of all display_y elements in the form of an array
     ypoints = np.array(display_y)
-
+    # Reason for choosing arrays: From multiple sources, it's been recommended that 
     ax.plot(xpoints, ypoints, color = 'Black', marker = 'o', markersize = 0.01, linestyle = '-', linewidth = 0.8)
         
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
